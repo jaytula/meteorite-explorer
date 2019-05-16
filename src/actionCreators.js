3d8c05cdc507c0daa8acc0f4c89e9475b704d555
Jay T <jaytula@gmail.com>
2019-05-16 16:49:22 -0700

    Add setSearchResults action creator

---
+++ src/actionCreators.js
@@ -0,0 +1,3 @@
+export const setSearchResults = text => dispatch => {
+  console.log({ text });
+};
--- src/components/SearchPanel.jsx
+++ src/components/SearchPanel.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
 import SearchResults from "./SearchResults";
 
-function SearchPanel() {
+import { setSearchResults } from "../actionCreators";
+
+function SearchPanel({ dispatchSetSearchResults }) {
+  const [text, setText] = useState("");
+
+  function onSubmit(e) {
+    e.preventDefault();
+    dispatchSetSearchResults(text);
+  }
+
   return (
     <div>
-      <input type="text" />
-      <button type="submit">Search</button>
-      <SearchResults />
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
+        <button type="submit">Search</button>
+        <SearchResults />
+      </form>
     </div>
   );
 }
 
-export default SearchPanel;
+const mapDispatchToProps = {
+  dispatchSetSearchResults: setSearchResults,
+};
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(SearchPanel);
