7d00af16e1a8fab40ada3da2aaba1b990d778f11
Jay T <jaytula@gmail.com>
2019-05-16 23:02:04 -0700

    Map through each Meteorite result in Search Results

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
+import Meteorite from "./Meteorite";
 
-function SearchResults() {
+function SearchResults({ results }) {
   return (
     <div>
       <h2>Search Results</h2>
+      <ul>
+        {results.map(meteor => (
+          <Meteorite key={meteor.id} data={meteor} />
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default SearchResults;
+const mapStateToProps = ({ search }) => ({
+  results: search.results,
+});
+
+export default connect(mapStateToProps)(SearchResults);
