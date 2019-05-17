6de88d9a53f3342c0a39b0bc7aba1a057f1bf57c
Jay T <jaytula@gmail.com>
2019-05-17 14:23:45 -0700

    Use localStorage to save search term and results

--- client/src/searchReducer.js
+++ client/src/searchReducer.js
@@ -1,11 +1,30 @@
 import { SET_RESULTS, CLEAR_RESULTS } from "./actionTypes";
 
-const searchReducer = (state = { results: [] }, action) => {
+const LS_KEY = "meteorite_search";
+
+const clearedState = { term: "", results: [] };
+let defaultState;
+
+try {
+  const data = localStorage.getItem(LS_KEY);
+  if (!data) throw new Error(`Key ${LS_KEY} not truthy`);
+  defaultState = JSON.parse(data);
+} catch (err) {
+  defaultState = { term: null, results: [] };
+}
+
+const searchReducer = (state = defaultState, action) => {
   switch (action.type) {
     case SET_RESULTS:
-      return { term: action.payload.term, results: action.payload.results };
+      const newState = {
+        term: action.payload.term,
+        results: action.payload.results,
+      };
+      localStorage.setItem(LS_KEY, JSON.stringify(newState));
+      return newState;
     case CLEAR_RESULTS:
-      return { term: "", results: [] };
+      localStorage.setItem(LS_KEY, JSON.stringify(clearedState));
+      return newState;
     default:
       return state;
   }
