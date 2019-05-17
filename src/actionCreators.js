2eb72879c39234fe46c5362a3eac30abe3937648
Jay T <jaytula@gmail.com>
2019-05-16 23:00:56 -0700

    Implement action creator setSearchResults

--- src/actionCreators.js
+++ src/actionCreators.js
@@ -1,3 +1,20 @@
-export const setSearchResults = text => dispatch => {
-  console.log({ text });
+import axios from "axios";
+
+import { SET_RESULTS } from "./actionTypes";
+const { BACKEND_ENDPOINT } = process.env;
+
+export const setSearchResults = text => async dispatch => {
+  try {
+    const response = await axios.get(BACKEND_ENDPOINT, {
+      params: {
+        $where: `lower(name) like lower('%${text}%')`,
+      },
+    });
+
+    const { data, status, statusText } = response;
+    if (status !== 200) throw new Error(statusText);
+    dispatch({ type: SET_RESULTS, payload: data });
+  } catch (err) {
+    console.error(err);
+  }
 };
