a237ac76cab85676e15a4e9b27e7555d724ee11a
Jay T <jaytula@gmail.com>
2019-05-17 14:35:18 -0700

    Update Meteorite to contain all fields

--- client/src/components/Meteorite.jsx
+++ client/src/components/Meteorite.jsx
@@ -1,9 +1,41 @@
 import React from "react";
 
+const RowHeader = (
+  <div>
+    <li>Name</li>
+    <li>Id</li>
+    <li>Name Type</li>
+    <li>Rec Class</li>
+    <li>Mass</li>
+    <li>Fall</li>
+    <li>Year</li>
+    <li>Lat</li>
+    <li>Long</li>
+  </div>
+);
+
 function Meteorite({ data }) {
+  const {
+    name,
+    id,
+    nametype,
+    recclass,
+    mass,
+    fall,
+    year,
+    reclat,
+    reclong,
+  } = data;
   return (
     <div>
-      <li>A meteorite</li>
+      <li>{name}</li> <li>{id}</li>
+      <li>{nametype}</li>
+      <li>{recclass}</li>
+      <li>{mass}</li>
+      <li>{fall}</li>
+      <li>{year}</li>
+      <li>{reclat}</li>
+      <li>{reclong}</li>
     </div>
   );
 }
